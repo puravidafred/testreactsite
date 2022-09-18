04f7370af1db9874876664af2706ba231955c36c
fhuss009 <fhuss009@fiu.edu>
2022-09-18 14:49:36 -0500

    Adding analytics stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import './App.css';
+ 
 import Button from '@mui/material/Button';
+import ReactGA from 'react-ga';
 
-function App() {
+
+const TRACKING_ID = 'UA-241667528-1';
+
+ReactGA.initialize(TRACKING_ID);
+
+const useAnalyticsEventTracker = (category="Blog category") => {
+  const eventTracker = (action = "test action", label = "test label") => {
+    ReactGA.event({category, action, label});
+  }
+  return eventTracker;
+}
+
+const App = () => {
+  const gaEventTracker = useAnalyticsEventTracker('A Button Clicks')
   return (
     <div className="App">
-      <Button variant="contained">A - Click Me!</Button>
+      <Button variant="contained">
+        <a href="#" onClick={() => gaEventTracker}>A - Click Me!</a>
+      </Button>
       <Button>B - Click Me!</Button>
       <Button variant="contained">C - Click Me!</Button>
     </div>
